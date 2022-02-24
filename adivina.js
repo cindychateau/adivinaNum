736b212f521378be7405c942d7513b6850fb170e
Cynthia Castillo <ca.castillo@gmail.com>
2022-02-24 07:11:28 -0600

    Algoritmo Adivina Numero2

---
+++ adivina.js
@@ -0,0 +1,12 @@
+//Generamos número aleatorio
+var numeroAleatorio = 0;
+numeroAleatorio = Math.floor( Math.random() * 101 );
+console.log( numeroAleatorio );
+
+//Indicar en la clase resultado si el número que intentaron adivinar
+//es mayor, menor o igual al número aleatorio
+function adivina() {
+    let resultado = document.querySelector( '.resultado' );
+    
+
+}
